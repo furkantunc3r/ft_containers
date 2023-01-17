b06103878c6cfae090656c3f8720a551aa227439
Furkan Tuncer <ftuncer@k2m10s03.42kocaeli.com.tr>
2023-01-17 17:00:42 +0300

    vectorHalfway

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include "vector.hpp"
 #include <vector>
 
+void pointer_func(const int* p, std::size_t size)
+{
+    std::cout << "data = ";
+    for (std::size_t i = 0; i < size; ++i)
+        std::cout << p[i] << ' ';
+    std::cout << '\n';
+}
+
 int main(void)
 {
-    std::vector<int> a(5, 31, std::allocator<int>());
+    std::cout << std::endl;
+    std::vector<int> a;
     ft::vector<int> ft_a;
-    ft::vector<int> ft_b(std::allocator<int>);
-    // ft::vector<int> ft_c(5);
-    a.assign(31, 31);
-    std::cout << a[31] << std::endl;
+
+    std::cout << "Initially, numbers.empty(): " << a.empty() << '\n';
+    std::cout << "Initially, numbers.empty(): " << ft_a.empty() << '\n';
+    
+    a.assign(5, 5);
+    std::cout << "After adding elements, numbers.empty(): " << a.empty() << '\n';
+    ft_a.assign(5, 5);
+    std::cout << "After adding elements, numbers.empty(): " << ft_a.empty() << '\n';
+    
+    std::cout << a.at(2) << std::endl;
+    std::cout << a[3] << std::endl;
+    std::cout << a.front() << std::endl;
+    std::cout << a.back() << std::endl;
+    pointer_func(a.data(), a.size());
+    std::cout << a.max_size() << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << ft_a.at(2) << std::endl;
+    std::cout << ft_a[3] << std::endl;
+    std::cout << ft_a.front() << std::endl;
+    std::cout << ft_a.back() << std::endl;
+    pointer_func(ft_a.data(), ft_a.size());
+    std::cout << ft_a.max_size() << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -5,7 +5,7 @@
 
 namespace ft
 {
-    template <typename _Tp, typename _Alloc = std::allocator<_Tp> >
+    template < typename _Tp, typename _Alloc = std::allocator<_Tp> >
     class vector
     {
         public :
@@ -23,19 +23,129 @@ namespace ft
             pointer _end;
             pointer _end_of_storage;
 
-            vector() ;
+            vector() : _begin(0), _end(0), _end_of_storage(0) {};
             explicit vector (const allocator_type& alloc) : _allocator(alloc) {};
             explicit vector (size_type n, const _Tp& value = _Tp(), const allocator_type& alloc = allocator_type()) : _allocator(alloc)
             {
                 this->_begin = this->_allocator.allocate(n);
                 this->_end = this->_begin;
-                for (int i = 0; i < n; i++)
+                for (size_type i = 0; i < n; i++)
                 {
                     this->_allocator.construct(_end, value);
                     _end++;
                 }
                 this->_end_of_storage = this->_begin + n;
             }
+            template<class InputIt>
+            vector (InputIt first, InputIt last, const allocator_type& alloc = allocator_type()) : _allocator(alloc)
+            {
+                size_type i = 0;
+                for (InputIt temp = first; temp != last; temp++)
+                    i++;
+                this->_begin = this->_allocator.allocate(i);
+                this->_end = this->_begin;
+                while (first != last)
+                {
+                    this->_allocator.construct(_end, *first);
+                    first++;
+                    _end++;
+                }
+                this->_end_of_storage = this->_begin + i;
+            }
+            vector (const vector& other) : _allocator(other._allocator)
+            {
+                this->_begin = this->_allocator(other.capacity());
+                this->_end = this->_begin;
+                this->_end_of_storage = this->_begin + (other.capacity());
+                for (pointer _temp = other->_begin; _temp != other->_end; _temp++)
+                {
+                    this->_allocator.construct(_end, _temp);
+                    _end++;
+                }
+            }
+            ~vector() {};
+            vector& operator= (const vector& other)
+            {
+                for (pointer _temp = this->_begin; _temp <= this->_end_of_storage; _temp++)
+                    this->_allocator.destroy(_temp);
+                this->_allocator.deallocate(this->_begin, capacity());
+                this->_begin = this->_allocator.allocate(other.capacity());
+                this->_end = this->_begin;
+                for (pointer _tempOther = other._begin; _tempOther <= other._end_of_storage; _tempOther++)
+                {
+                    this->_allocator.construct(_end, _tempOther);
+                    _end++;
+                }
+                this->_end_of_storage = this->_begin + (other.capacity());
+                return *this;
+            }
+            void assign(size_type count, const _Tp& value)
+            {
+                if (count > this->capacity())
+                {
+                    for (pointer _temp = this->_begin; _temp <= this->_end_of_storage; _temp++)
+                        this->_allocator.destroy(_temp);
+                    this->_allocator.deallocate(this->_begin, capacity());
+                    this->_begin = this->_allocator.allocate(count);
+                    this->_end = this->_begin;
+                    for (size_type i = 0; i < count; i++)
+                    {
+                        this->_allocator.construct(_end, value);
+                        _end++;
+                    }
+                    this->_end_of_storage = this->_begin + count;
+                }
+            }
+            allocator_type get_allocator() const { return this->_allocator; }
+            
+            // element access
+
+            reference at(size_type pos)
+            {
+                if (pos > capacity())
+                    throw std::out_of_range("Out of bounds!");
+                return reference(this->_begin[pos]);
+            };
+            const_reference at(size_type pos) const
+            {
+                if (pos > capacity())
+                    throw std::out_of_range("Out of bounds!");
+                return const_reference(this->_begin[pos]);
+            };
+
+            reference operator[] (size_type pos) { return reference(this->_begin[pos]); }
+            const_reference operator[] (size_type pos) const { return const_reference(this->_begin + pos); }
+
+            reference front() { return reference(*this->_begin); }
+            const_reference front() const { return const_reference(*this->_begin); }
+
+            reference back() { return reference(*(this->_end - 1)); }
+            const_reference back() const { return const_reference(*(this->_end - 1)); }
+
+            _Tp* data() { return pointer(this->_begin); }
+            const _Tp* data() const { return const_pointer(this->_begin); }
+
+            // capacity
+
+            bool empty() const
+            {
+                if (this->_begin == this->_end)
+                    return 1;
+                return 0;
+            }
+            size_type size() const { return size_type(this->_end - this->_begin); }
+            size_type capacity() const { return size_type(this->_end_of_storage - this->_begin); }
+            size_type max_size() const { return this->_allocator.max_size(); }
+            void reserve (size_type new_cap)
+            {
+                if (new_cap > max_size())
+                    throw std::length_error("Max size exceeded!");
+                if (new_cap > capacity())
+                {
+                    
+                }
+            }
+
     };
 }
 
