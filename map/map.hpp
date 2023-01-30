2dd5ef226ca14eb3212a9bc6c5c6db97a5e2055f
Furkan Tuncer <ftuncer@k2m09s03.42kocaeli.com.tr>
2023-01-30 16:36:50 +0300

    mapBegin

---
+++ map/map.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace ft
+{
+    template <
+        class Key,
+        class T,
+        class Compare = std::less<Key>,
+        class Allocator = std::allocator<std::pair<const Key, T>>
+    > class map
+    {
+        
+    };
+};
